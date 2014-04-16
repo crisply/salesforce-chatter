a18ad88cc2f5f80a9add6319c9d357ee94140163
Dan Wanek <dan@crisply.com>
2014-04-16 15:12:39 +0000

    Simplify Feed API. Will add POST/PUT back eventually

--- lib/salesforce/chatter/api/feed_items.rb
+++ lib/salesforce/chatter/api/feed_items.rb
@@ -5,26 +5,14 @@ module Salesforce::Chatter
 
       # @see http://www.salesforce.com/us/developer/docs/chatterapi/Content/connect_resource_feeds_news.htm
       # @param opts [Hash] options for Salesforce
-      def my_feed_items(opts = {})
-        response = get('/chatter/feeds/news/me/feed-items', opts)
+      def news_feed(user_id: :me, opts: {})
+        get("/chatter/feeds/news/#{user_id}/feed-items", opts)
       end
 
-      def my_feed_item_create(text, options={})
-        options[:text] = text
-        response = post("/chatter/feeds/news/me/feed-items", options)
+      def user_profile_feed(user_id: :me, opts: {})
+        get("/chatter/feeds/user-profile/#{user_id}/feed-items", opts)
       end
 
-      def my_feed_is_modified(opts = {})
-        response = get("/chatter/feeds/news/me/is-modified", opts)
-      end
-
-      def feed_item_destroy(id, options={})
-        response = delete("/chatter/feed-items/#{id}", options)
-      end
-
-      def feed_item(id, options={})
-        response = get("/chatter/feed-items/#{id}", options)
-      end
     end
   end
 end
